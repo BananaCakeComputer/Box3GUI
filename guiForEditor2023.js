2bb2310d570a54aec7a3a353fd6f1d054f871047
蕉饼 <77569325+BananaCakeComputer@users.noreply.github.com>
2023-07-11 05:46:25 +0800

    Create guiForEditor2023.js

---
+++ guiForEditor2023.js
@@ -0,0 +1,181 @@
+var guiSpace = {1:false ,2:false ,3:false ,4:false ,5:false}
+var guiBackground = 0
+var guiPublicItems = {}
+var guiPublicItemsNames = []
+var publicIsPress = false;
+var publicHasBtn = false
+class Box3GUI {
+    constructor(entity){
+        this.entity = entity
+        if(entity!='world'){
+            for(let i in guiSpace){
+                if(!guiSpace[i]){
+                    guiSpace[i] = entity.player.name
+                    break
+                }
+            }
+        }
+    }
+    enterPublicGUI(){
+        if(this.entity=='world'){
+            world.say('无法给世界使用GUI')
+            return;
+        }
+        this.entity.guiBtnI = 0
+        this.entity.isGuiMode = true;
+        this.entity.guiSpace = 'public';
+        this.entity.player.cameraMode = "fixed";
+        this.entity.player.cameraPosition = new GameVector3(6.8, 4, 6.5)
+        this.entity.player.cameraTarget = new GameVector3(1, 4, 6.5)
+    }
+    exitPublicGUI(){
+        if(this.entity=='world'){
+            world.say('无法给世界使用GUI')
+            return;
+        }
+        this.entity.guiSpace = false;
+        this.entity.isGuiMode = false;
+        this.entity.player.cameraMode = "follow";
+    }
+    publicBackground(meshID){
+        if(guiBackground == 0){
+            guiBackground = world.createEntity({
+                mesh: meshID,
+                position:new GameVector3(2,4,6.5),
+                meshScale:new GameVector3(0.065, 0.065, 0.065),
+                collides:false,
+                fixed:true,
+                gravity:false,
+            })
+        }else{
+            guiBackground.destroy()
+            guiBackground = world.createEntity({
+                mesh: meshID,
+                position:new GameVector3(2.1,4,6.5),
+                meshScale:new GameVector3(0.065, 0.065, 0.065),
+                collides:false,
+                fixed:true,
+                gravity:false,
+            })
+        }
+    }
+    publicItemAdd(name, position, meshID, type){
+        guiPublicItems[name] = [
+            position,
+            world.createEntity({
+                mesh: meshID,
+                position:new GameVector3(2.1,5-(position[1]/16),10-(position[0]/16)),
+                meshScale:new GameVector3(0.065, 0.065, 0.065),
+                collides:false,
+                fixed:true,
+                gravity:false,
+            }),
+            type
+        ]
+        guiPublicItemsNames.push(name)
+    }
+    publicItemDelete(name){
+        guiPublicItems[name][1].destroy()
+        delete guiPublicItems[name]
+        for(var i = 0;i < guiPublicItemsNames.length;i++){
+            if(guiPublicItemsNames[i] == name){
+                guiPublicItemsNames.splice(i,1)
+                return
+            }
+        }
+    }
+    publicOnPress(button){
+        for(let entity of world.querySelectorAll('player')){
+            if(button === entity.buttonPress){
+                return true
+            }
+        }
+        return false
+    }
+    publicItemScale(item,x,y){
+        guiPublicItems[item][1].meshScale = new GameVector3(0.065, y*0.065, x*0.065);
+    }
+    publicTint(color){
+        for(var z = 2;z <= 9;z++){
+            voxels.setVoxel(2,1,z,color+'_light');
+            voxels.setVoxel(2,6,z+1,color+'_light');
+        }
+        for(var z = 2;z <= 10;z++){
+            voxels.setVoxel(3,1,z,color+'_light');
+            voxels.setVoxel(3,6,z,color+'_light');
+        }
+        for(var y = 2;y <= 5;y++){
+            voxels.setVoxel(2,y,1,color+'_light');
+            voxels.setVoxel(2,y,11,color+'_light');
+        }
+    }
+}
+world.gui = new Box3GUI('world')
+world.onPlayerJoin(({entity})=>{
+    entity.isGuiMode = false;
+    entity.gui = new Box3GUI(entity)
+    entity.guiSpace = false
+    entity.buttonPress = false;
+    entity.guiOnPressHasBeenFinish = false
+    world.onTick(()=>{
+        if(entity.guiSpace!='public'){
+            return
+        }
+        if(guiPublicItemsNames.length==0){
+            return
+        }
+        if(entity.guiBtnI==0 && guiPublicItems[guiPublicItemsNames[0]][2]==='img'){
+            return
+        }else{
+            for(var i = 0;i <= (guiPublicItemsNames.length-1);i++){
+                guiPublicItems[guiPublicItemsNames[i]][1].meshColor = new GameRGBAColor(1, 1, 1, 1)
+            }
+            publicHasBtn = false
+            for(var i = 0;i <= (guiPublicItemsNames.length-1);i++){
+                if(guiPublicItems[guiPublicItemsNames[i]][2]='btn'){
+                    publicHasBtn = true
+                }
+            }
+            if(!publicHasBtn){
+                return
+            }
+            if(!entity.buttonPress){
+                guiPublicItems[guiPublicItemsNames[entity.guiBtnI]][1].meshColor = new GameRGBAColor(1, 1, 0, 1)
+            }else{
+                guiPublicItems[guiPublicItemsNames[entity.guiBtnI]][1].meshColor = new GameRGBAColor(0.7, 0.7, 0, 1)
+            }
+        }
+    })
+})
+world.onPress(({button, entity})=>{
+    if(entity.isGuiMode){
+        if(button==='action0'){
+            entity.guiOnPressHasBeenFinish = false
+            for(var i = entity.guiBtnI;i < (guiPublicItemsNames.length - 1);i++){
+                entity.guiBtnI += 1
+                if(guiPublicItems[guiPublicItemsNames[i]][2]==='btn'){
+                    entity.guiOnPressHasBeenFinish = true
+                    break
+                }
+            }
+            if(!entity.guiOnPressHasBeenFinish){
+                entity.guiBtnI = 0
+            }
+        }else if(button==='action1'){
+            entity.buttonPress = guiPublicItemsNames[entity.guiBtnI]
+        }
+    }
+})
+world.onRelease(({button, entity}) => {
+    if(button == 'action1'){
+        entity.buttonPress = false;
+    }
+})
+world.onPlayerLeave(({entity}) => {
+    for(let i in guiSpace){
+        if(guiSpace[i]==entity.player.name){
+            guiSpace[i] = false
+            break
+        }
+    }
+})
